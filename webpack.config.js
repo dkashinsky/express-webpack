c6e90be27fefc1bfdf70632f64e8add63479ef9f
Dmitry Kashinsky <dmitry.kashinsky@gmail.com>
2021-04-21 00:39:54 +0400

    configure webpack build

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = () => {
+    const config = {
+        entry: './src/greet.ts',
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: 'greet.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: 'awesome-typescript-loader'
+                }
+            ],
+        }
+    };
+
+    return config;
+};
